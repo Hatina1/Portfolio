df4069d54996d6b8507949a20a9723ab53ddce78
Hata Couibaly <hata.coulibaly@yahoo.fr>
2022-06-04 22:25:12 +0200

    Modify navbar styles

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,12 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "../styles/Navbar.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+	const router = useRouter();
+
 	return (
 		<div className={styles.navbar}>
 			<div className={styles.name}>Hata COULIBALY </div>
@@ -11,9 +14,33 @@ const Navbar = () => {
 				<a href="/">
 					<FontAwesomeIcon icon={faHome} className={styles.icon} />
 				</a>{" "}
-				<Link href="/projets">Projets</Link>
-				<Link href="/parcours">Parcours</Link>
-				<Link href="/contact">Contact</Link>
+				<Link href="/projets" passHref>
+					<a
+						className={
+							router.pathname == "/projets" ? styles.active : styles.category
+						}
+					>
+						Projets
+					</a>
+				</Link>
+				<Link href="/parcours" passHref>
+					<a
+						className={
+							router.pathname == "/parcours" ? styles.active : styles.category
+						}
+					>
+						Parcours
+					</a>
+				</Link>
+				<Link href="/contact" passHref>
+					<a
+						className={
+							router.pathname == "/contact" ? styles.active : styles.category
+						}
+					>
+						Contact
+					</a>
+				</Link>
 			</div>
 		</div>
 	);
