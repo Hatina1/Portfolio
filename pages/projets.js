123bb37473c3b10fe871d2e00476029abee1422e
Hata Couibaly <hata.coulibaly@yahoo.fr>
2022-06-04 21:07:41 +0200

    Change path file

--- pages/projets.js
+++ pages/projets.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Projects.module.css";
-const path = "/../public/images/";
+const path = "/images/";
 
 export default function Projets() {
 	return (
