11cad2d64650e8b27e193dc00c31723e28a42992
Hata Couibaly <hata.coulibaly@yahoo.fr>
2022-06-15 09:21:36 +0200

    Insert social media icons

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,11 @@
 import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Contact.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { useState } from "react";
 
 export default function Contact() {
 	return (
@@ -13,38 +18,40 @@ export default function Contact() {
 			<main className={styles.main}>
 				<section className={styles.section}>
 					<h1 className={styles.title}>Contact</h1>
-					<ul className={styles.contactlist}>
-						<li>
-							<h2 className={styles.contact}>
-								GitHub :{" "}
-								<Link
-									href="https://github.com/Hatina1 "
-									className={styles.links}
-								>
-									https://github.com/Hatina1
-								</Link>
-							</h2>
-						</li>
-						<li>
-							<h2 className={styles.contact}>
-								LinkedIn :{" "}
-								<Link
-									href="https://www.linkedin.com/in/hata-c-b15168a5/"
-									className={styles.links}
-								>
-									https://www.linkedin.com/hata_coulibaly
-								</Link>
-							</h2>
-						</li>
-						<li>
-							<h2 className={styles.contact}>
-								E-mail :{" "}
-								<Link href="hata.coulibaly@yahoo.fr" className={styles.links}>
-									hata.coulibaly@yahoo.fr
-								</Link>
-							</h2>
-						</li>
-					</ul>
+					<section className={styles.section_container}>
+						<ul className={styles.contactlist}>
+							<li>
+								<h2 className={styles.contact}>
+									<FontAwesomeIcon icon={faGithub} className={styles.icon} />{" "}
+									<Link
+										href="https://github.com/Hatina1 "
+										className={styles.links}
+									>
+										https://github.com/Hatina1
+									</Link>
+								</h2>
+							</li>
+							<li>
+								<h2 className={styles.contact}>
+									<FontAwesomeIcon icon={faLinkedin} className={styles.icon} />{" "}
+									<Link
+										href="https://www.linkedin.com/in/hata-c-b15168a5/"
+										className={styles.links}
+									>
+										https://www.linkedin.com/hata_coulibaly
+									</Link>
+								</h2>
+							</li>
+							<li>
+								<h2 className={styles.contact}>
+									<FontAwesomeIcon icon={faEnvelope} className={styles.icon} />{" "}
+									<Link href="hata.coulibaly@yahoo.fr" className={styles.links}>
+										hata.coulibaly@yahoo.fr
+									</Link>
+								</h2>
+							</li>
+						</ul>
+					</section>
 				</section>
 			</main>
 		</div>
