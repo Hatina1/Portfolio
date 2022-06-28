263de5a88a191e2978bc721411884b9c705398b3
Hata Couibaly <hata.coulibaly@yahoo.fr>
2022-06-28 13:02:32 +0200

    Add description paragraphs to projects

--- pages/projets.js
+++ pages/projets.js
@@ -21,13 +21,16 @@ export default function Projets() {
 							src={`${path}Groupomania.png`}
 							classname={styles.card_images}
 							alt="groupomania"
-							width={330}
-							height={180}
+							width={310}
+							height={250}
 						/>
 						<figcaption className={styles.card_text}>
 							<h2 className={styles.card_title}>Groupomania</h2>
 							<p className={styles.card_desc}>
 								Création d'une social app d'entreprise
+							</p>
+							<p className={styles.card_tools_desc}>
+								NodeJS, Express, Sequelize, MySQL, ReactJS, Bootstrap, Giphy API
 							</p>
 						</figcaption>
 					</figure>
@@ -36,14 +39,15 @@ export default function Projets() {
 							src={`${path}Kanap.png`}
 							classname={styles.card_images}
 							alt="Kanap"
-							width={330}
-							height={180}
+							width={310}
+							height={250}
 						/>
 						<figcaption className={styles.card_text}>
 							<h2 className={styles.card_title}>Kanap</h2>
 							<p className={styles.card_desc}>
 								Création d'un site de e-commerce dynamique
 							</p>
+							<p className={styles.card_tools_desc}>HTML, CSS, Javascript</p>
 						</figcaption>
 					</figure>
 					<figure className={styles.card}>
@@ -51,14 +55,15 @@ export default function Projets() {
 							src={`${path}Oh-My-Food.png`}
 							classname={styles.card_images}
 							alt="Oh My Food"
-							width={330}
-							height={180}
+							width={310}
+							height={250}
 						/>
 						<figcaption className={styles.card_text}>
 							<h2 className={styles.card_title}>Oh My Food</h2>
 							<p className={styles.card_desc}>
 								Dynamisation d'une page web avec des animations CSS
 							</p>
+							<p className={styles.card_tools_desc}>HTML, CSS, SASS</p>
 						</figcaption>
 					</figure>
 					<figure className={styles.card}>
@@ -66,14 +71,15 @@ export default function Projets() {
 							src={`${path}Booki.png`}
 							classname={styles.card_images}
 							alt="Booki"
-							width={330}
-							height={180}
+							width={310}
+							height={250}
 						/>
 						<figcaption className={styles.card_text}>
 							<h2 className={styles.card_title}>Booki</h2>
 							<p className={styles.card_desc}>
 								Transformation d'une maquette en site web
 							</p>
+							<p className={styles.card_tools_desc}>HTML, CSS</p>
 						</figcaption>
 					</figure>
 					<figure className={styles.card}>
@@ -81,13 +87,16 @@ export default function Projets() {
 							src={`${path}Hot-Takes.png`}
 							classname={styles.card_images}
 							alt="Hot Takes"
-							width={330}
-							height={180}
+							width={310}
+							height={250}
 						/>
 						<figcaption className={styles.card_text}>
 							<h2 className={styles.card_title}>Hot Takes</h2>
 							<p className={styles.card_desc}>
 								Construction d'une API sécurisée
+							</p>
+							<p className={styles.card_tools_desc}>
+								MongoDB, NodeJS, Express, Javascript
 							</p>
 						</figcaption>
 					</figure>
@@ -96,14 +105,15 @@ export default function Projets() {
 							src={`${path}La-Chouette.png`}
 							classname={styles.card_images}
 							alt="La chouette"
-							width={330}
-							height={180}
+							width={310}
+							height={250}
 						/>
 						<figcaption className={styles.card_text}>
 							<h2 className={styles.card_title}>La Chouette</h2>
 							<p className={styles.card_desc}>
 								Optimisation d'un site web existant
 							</p>
+							<p className={styles.card_tools_desc}>HTML, CSS, SEO</p>
 						</figcaption>
 					</figure>
 				</section>
